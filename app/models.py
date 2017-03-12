58829ec7a5cf50ea477f74e7c662f7e23997e542
RheeJ <JoonyongRhee2016@u.northwestern.edu>
2017-03-11 22:56:33 -0600

    add relational model

--- app/models.py
+++ app/models.py
@@ -1,9 +1,18 @@
 from app import db
 
+class Fridge(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), index=True, unique=True)
+	foods = db.relationship('FoodItem', lazy='dynamic')
+	def __repr__(self):
+		return '<Fridge %r>' % (self.name)
+
 class FoodItem(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(64), index=True)
 	calories = db.Column(db.Integer)
 	quantity = db.Column(db.Integer)
+	fridge_id = db.Column(db.Integer, db.ForeignKey('fridge.id'))
+	fridge = db.relationship(Fridge)
 	def __repr__(self):
 		return '<Food %r>' % (self.name)
--- app/views.py
+++ app/views.py
@@ -6,42 +6,30 @@ import json
 @app.route('/add', methods=['POST'])
 def add_to_fridge():
 	data = request.get_json()
-	if not hasattr(data, '__iter__'):
+	if not hasattr(data['foods'], '__iter__'):
 		return Response(status=400)
-	for food in data:
-		obj = FoodItem.query.filter_by(name=food['name'])
+	fridge = FoodItem.query.filter_by(name=data['fridge'])
+	if not fridge.count():
+		return Response(status=400)
+	for food in data['foods']:
+		obj = FoodItem.query.filter_by(name=food['name'], fridge=fridge)
 		if not obj.count():
-			new_food = models.FoodItem(name=food['name'], calories=food['calories'], quantity=food['quantity'])
+			new_food = models.FoodItem(name=food['name'], calories=food['calories'], quantity=food['quantity'], fridge=fridge)
 			db.session.add(new_food)
 		else:
 			obj.quantity += food['quantity']
 	db.session.commit()
 	return Response("New Items Have Been Added To Fridge", status=200)
 
-@app.route('/contents', methods=['GET'])
+@app.route('/contents', methods=['POST'])
 def fridge_content():
 	food_list = []
-	for food in FoodItem.query.all():
+	data = request.get_data()
+	fridge = Fridge.query.filter_by(name=data['fridge'])
+	if not fridge.count():
+		return Response(status=400)
+	for food in FoodItem.query.filter_by(fridge=fridge):
 		food_list.append({ 'name' : food.name, 'quantity' : food.quantity })
 	return Response(json.dumps(food_list), status=200)
-
-@app.route('/remove', methods=['POST'])
-def remove_from_fridge():
-	data = request.get_json()
-	if not hasattr(data, '__iter__'):
-		return Response(status=400)
-	for food in data:
-		obj = FoodItem.query.filter_by(name=food['name'])
-		if obj.count():
-			if food['quantity'] < obj.quantity:
-				obj.quantity -= food['quantity']
-				db.session.commit()
-			elif food['quantity'] == obj.quantity:
-				obj.delete()
-				db.session.commit()
-			else:
-				return Response(status=400)
-		else:
-			return Response(status=400)
 
 	
