4249cbc6630f367556ae3d8b898c41254d4c51f5
RheeJ <JoonyongRhee2016@u.northwestern.edu>
2017-03-11 20:57:37 -0600

    Database schema versioning

--- app/models.py
+++ app/models.py
@@ -2,7 +2,8 @@ from app import db
 
 class FoodItem(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(64), index=True, unique=True)
-	calories = db.Column(db.Integer, index=True)
+	name = db.Column(db.String(64), index=True)
+	calories = db.Column(db.Integer)
+	quantity = db.Column(db.Integer)
 	def __repr__(self):
 		return '<Food %r>' % (self.name)
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app, models
 from flask import request, Response, Flask
 from models import *
 import json
@@ -9,9 +9,12 @@ def add_to_fridge():
 	if not hasattr(data, '__iter__'):
 		return Response(status=400)
 	for food in data:
-		if not FoodItem.query.filter_by(name=food['name']).count():
-			new_food = models.FoodItem(name=food['name'], calories=food['calories'])
+		obj = FoodItem.query.filter_by(name=food['name'], expiration_date=food['expiration_date'])
+		if not obj.count():
+			new_food = models.FoodItem(name=food['name'], calories=food['calories'], expiration_date=food['expiration_date'], quantity=0)
 			db.session.add(new_food)
+		else:
+			obj.quantity += 1
 	db.session.commit()
 
 	return Response("New Items Have Been Added To Fridge", status=200)
